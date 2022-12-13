6f27c23718ede25d14a59911f0e0fda23c6037f5
Álvaro Renan <alvaro.carvalho.703@ufrn.edu.br>
2022-12-13 00:54:55 -0300

    send whatsapp msg + location + visual

--- core/models.py
+++ core/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 # Create your models here.
 
 class Evento(models.Model):
     nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20, null=True, blank=True)
+    endereco = models.CharField(max_length=200, null=True, blank=True)
     descricao = models.TextField(blank=True, null=True)
     data = models.DateTimeField(verbose_name='Data do Evento')
     created_at = models.DateTimeField(auto_now=True, verbose_name='Criado em')
@@ -22,3 +25,17 @@ class Evento(models.Model):
 
     def get_data_input_evento(self):
         return self.data.strftime('%Y-%m-%dT%H:%M')
+
+    def get_evento_atrasado(self):
+        if self.data < datetime.now():
+            return True
+        else:
+            return False
+
+    def get_telefone_whitout_mask(self):
+        return self.telefone.replace('(', '').replace(')', '').replace('-', '').replace(' ', '')
+
+    def get_dia(self):
+        return self.data.strftime('%d/%m/%Y')
+    def get_hora(self):
+        return self.data.strftime('%H:%M')
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import User
+from django.http import Http404, JsonResponse
 from django.shortcuts import render, redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from datetime import datetime, timedelta
 # Create your views here.
 
 def login_user(request):
@@ -23,7 +26,9 @@ def submit_login(request):
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
+    data_atual = datetime.now() - timedelta(hours=1)
+    evento = Evento.objects.filter(usuario=usuario,
+                                   data__gt=data_atual)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
@@ -43,17 +48,23 @@ def logout_user(request):
 def submit_evento(request):
     if request.POST:
         nome = request.POST.get('nome')
+        telefone = request.POST.get('telefone')
+        endereco = request.POST.get('endereco')
         data = request.POST.get('data')
         descricao = request.POST.get('descricao')
         usuario = request.user
         id_evento = request.POST.get('id_evento')
         if id_evento:
             Evento.objects.filter(id=id_evento).update(nome=nome,
+                                                        telefone=telefone,
+                                                        endereco=endereco,
                                                       data=data,
                                                       descricao=descricao)
         else:
             Evento.objects.create(nome=nome,
                                   data=data,
+                                    telefone=telefone,
+                                    endereco=endereco,
                                   descricao=descricao,
                                   usuario=usuario)
     return redirect('/')
@@ -61,7 +72,19 @@ def submit_evento(request):
 @login_required(login_url='/login/')
 def delete_evento(request, id_evento):
     usuario = request.user
-    evento = Evento.objects.get(id=id_evento)
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
     if usuario == evento.usuario:
         evento.delete()
+    else:
+        raise Http404()
     return redirect('/')
+
+def json_lista_evento(request, id_usuario):
+    usuario = User.objects.get(id=id_usuario)
+    evento = Evento.objects.filter(usuario=usuario).values('id', 'nome', 'descricao')
+    return JsonResponse(list(evento), safe=False)
+
+
